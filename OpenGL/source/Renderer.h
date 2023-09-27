78b6968e1050a8f863099499fb410b5d996bb9b0
Seb <sebbooth@yahoo.ca>
2023-09-26 21:03:03 -0700

    renderer abstraction

--- OpenGL/source/Renderer.h
+++ OpenGL/source/Renderer.h
@@ -2,6 +2,10 @@
 
 #include <GL/glew.h>
 
+#include "VertexArray.h"
+#include "IndexBuffer.h"
+#include "Shader.h"
+
 #define ASSERT(x) if (!(x))  __debugbreak();
 #define GLCall(x) GLClearError(); x; ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 
@@ -10,5 +14,9 @@ bool GLLogCall(const char* function, const char* file, int line);
 
 class Renderer
 {
+
+public:
+	void Clear() const;
+	void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
 };
 
--- OpenGL/source/main.cpp
+++ OpenGL/source/main.cpp
@@ -7,6 +7,7 @@
 
 #include "Renderer.h"
 #include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
@@ -77,11 +78,11 @@ int main(void)
        
 
         va.UnBind();
-        shader.UnBind();
         vb.UnBind();
         ib.UnBind();
-    
+        shader.UnBind();
 
+        Renderer renderer;
 
         float r = 0.0f;
         float increment = 0.05f;
@@ -89,17 +90,11 @@ int main(void)
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            GLCall(glClear(GL_COLOR_BUFFER_BIT));
-
+            renderer.Clear();
             shader.Bind();
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
            
-
-            va.Bind();
-            ib.Bind();
-
-            GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
-
+            renderer.Draw(va,ib,shader);
 
             if (r > 1.0f)
                 increment = -0.05f;
