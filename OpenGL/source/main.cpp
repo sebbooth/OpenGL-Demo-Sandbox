7d5639ac7c55442e4ca28d52334c1ccff1a761c4
Seb <sebbooth@yahoo.ca>
2023-09-28 18:52:37 -0700

    added imgui

--- OpenGL/source/main.cpp
+++ OpenGL/source/main.cpp
@@ -15,6 +15,8 @@
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
 
 int main(void)
 {
@@ -30,7 +32,7 @@ int main(void)
 
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -56,10 +58,10 @@ int main(void)
 
     {
         float positions[] = {
-            -0.5f, -0.5f, 0.0f, 0.0f,
-             0.5f, -0.5f, 1.0f, 0.0f,
-             0.5f,  0.5f, 1.0f, 1.0f,
-            -0.5f,  0.5f, 0.0f, 1.0f
+             0.0f,  0.0f, 0.0f, 0.0f,
+             200.0f,  0.0f, 1.0f, 0.0f,
+             200.0f,  200.0f, 1.0f, 1.0f,
+             0.0f,  200.0f, 0.0f, 1.0f
         };
 
         unsigned int indices[] = {
@@ -81,12 +83,12 @@ int main(void)
 
         IndexBuffer ib(indices, 6);
 
-        glm::mat4 proj = glm::ortho(-1.0f, 1.0f, -0.75f, 0.75f, -1.0f, 1.0f);
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
         Shader shader("resources/shaders/Basic.glsl");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
 
         Texture texture("resources/textures/kitten.png");
         texture.Bind();
@@ -101,6 +103,17 @@ int main(void)
 
         Renderer renderer;
 
+        ImGui::CreateContext();
+        ImGui_ImplGlfwGL3_Init(window, true);
+        ImGui::StyleColorsDark();
+
+
+        bool show_demo_window = true;
+        bool show_another_window = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+
+        glm::vec3 translation(0, 0, 0);
         float r = 0.0f;
         float increment = 0.05f;
         /* Loop until the user closes the window */
@@ -108,9 +121,16 @@ int main(void)
         {
             /* Render here */
             renderer.Clear();
+
+            ImGui_ImplGlfwGL3_NewFrame();
+
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+            glm::mat4 mvp = proj * view * model;
+
             shader.Bind();
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-           
+            shader.SetUniformMat4f("u_MVP", mvp);
+
             renderer.Draw(va,ib,shader);
 
             if (r > 1.0f)
@@ -119,6 +139,18 @@ int main(void)
                 increment = 0.05f;
             r += increment;
 
+
+
+            {
+                ImGui::SliderFloat3("float", &translation.x, 0.0f, 960.0f);
+                
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            }
+
+
+
+            ImGui::Render();
+            ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
@@ -126,8 +158,9 @@ int main(void)
             glfwPollEvents();
         }
     }
-        //glDeleteProgram(shader);
-        glfwTerminate();
+    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui::DestroyContext();
+    glfwTerminate();
     
 
     return 0;
